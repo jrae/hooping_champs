e1c47660a15d0786905ae205ae152be86a0e091c
Jason Rae <jrae@wearefuturegov.com>
2015-06-13 17:56:27 +0100

    Fixing image issue

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
 
+
   has_attached_file :image, :styles => { :large => "300x300#", :small => "150x150#" }
 
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 end
